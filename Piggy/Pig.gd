33c9705164cfd2984b8ced9c019e4fea09732484
Worajedt S <sjedt@3ddaily.com>
2021-12-27 17:33:26 +0700

    create new scene + anim with multi-frame
    1. create a specific scene for Piggy
    2. create animations (idle, run)
    3. rename Sprite -> PigSprite
    4. animation change when left <> right & idle

--- Piggy/Pig.gd
+++ Piggy/Pig.gd
@@ -2,17 +2,31 @@ extends Area2D
 
 #const SPEED = 0.1
 export(int) var SPEED = 200
+var moving:bool = false
+
+onready var animPlayer = $AnimationPlayer
+onready var sprite = $PigSprite
 
 func _process(delta):
+	moving = false
 	if Input.is_key_pressed(KEY_RIGHT): 		#get int of KEY in KeyList
+		sprite.flip_h = false
 		move(SPEED,0, delta)
 	if Input.is_action_pressed("ui_left"):	#get string of the mapped key name
+		sprite.flip_h = true
 		move(-SPEED,0, delta)
 	if Input.is_action_pressed("ui_up"):
 		move(0,-SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
 		move(0,SPEED, delta)
+		
+	if moving == true:
+		animPlayer.play("Run")
+	else:
+		animPlayer.play("Idle")
 	
 func move(xspeed, yspeed, delta):
+	moving = true
 	position.x += xspeed * delta
 	position.y += yspeed * delta
+	
