41e7b3589d20934aeb103c5fba255fd258838fb6
Worajedt S <sjedt@3ddaily.com>
2021-12-27 17:07:06 +0700

    add 'delta' and export SPEED to be more adjustable

--- Piggy/Pig.gd
+++ Piggy/Pig.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
-const SPEED = 0.1
+#const SPEED = 0.1
+export(int) var SPEED = 200
+
 func _process(delta):
 	if Input.is_key_pressed(KEY_RIGHT): 		#get int of KEY in KeyList
-		move(SPEED,0)
+		move(SPEED,0, delta)
 	if Input.is_action_pressed("ui_left"):	#get string of the mapped key name
-		move(-SPEED,0)
+		move(-SPEED,0, delta)
 	if Input.is_action_pressed("ui_up"):
-		move(0,-SPEED)
+		move(0,-SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
-		move(0,SPEED)
+		move(0,SPEED, delta)
 	
-func move(xspeed, yspeed):
-	position.x += xspeed
-	position.y += yspeed
+func move(xspeed, yspeed, delta):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
