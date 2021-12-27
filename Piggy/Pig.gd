878b9abd613b93af2b017fe0a682366537be0693
Worajedt S <sjedt@3ddaily.com>
2021-12-27 18:17:05 +0700

    Collission detection and score
    1. pig can collide with apple (eat)
    2. each time it eat, it grows up
    3. and get 1 more score

--- Piggy/Pig.gd
+++ Piggy/Pig.gd
@@ -3,12 +3,15 @@ extends Area2D
 #const SPEED = 0.1
 export(int) var SPEED = 200
 var moving:bool = false
+var score:int = 0
 
 onready var animPlayer = $AnimationPlayer
 onready var sprite = $PigSprite
+onready var score_text = $"../Score/CurrentScore"
 
 func _process(delta):
 	moving = false
+	score_text.text = str(score)
 	if Input.is_key_pressed(KEY_RIGHT): 		#get int of KEY in KeyList
 		sprite.flip_h = false
 		move(SPEED,0, delta)
@@ -24,9 +27,22 @@ func _process(delta):
 		animPlayer.play("Run")
 	else:
 		animPlayer.play("Idle")
+		
+	# you can detect collission here with get_overlapping_areas()
+	# OR use the _area_entered signal function 
+	#	var areas = get_overlapping_areas()
+	#	for area in areas:
+	#		area.queue_free()
 	
 func move(xspeed, yspeed, delta):
 	moving = true
 	position.x += xspeed * delta
 	position.y += yspeed * delta
 	
+
+
+func _on_Piggy_area_entered(area):
+	area.queue_free()
+	score += 1
+	score_text.text = str(score)
+	self.scale *= 1.15	# eat more apple to grow up!
